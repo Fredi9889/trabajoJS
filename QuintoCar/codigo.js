3bd631e813f0256bd66fe8aff3c3462f7a3c90d9
Iwaito0 <57145421+Iwaito0@users.noreply.github.com>
2019-10-30 11:39:10 +0100

    Add files via upload

---
+++ QuintoCar/codigo.js
@@ -0,0 +1,7 @@
+"use strict";
+var divListado = document.getElementById("listado");
+
+function altaCliente() {
+    frmAltaCliente.style.display = "block";
+    divListado.style.display = "none";
+}

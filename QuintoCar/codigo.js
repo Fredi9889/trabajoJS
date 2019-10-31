c763aab8495fbadddeb7dab05ddf989da1c2427e
Iwaito0 <57145421+Iwaito0@users.noreply.github.com>
2019-10-31 13:18:08 +0100

    Add files via upload

---
+++ QuintoCar/codigo.js
@@ -0,0 +1,36 @@
+"use strict";
+//Programa Princiapl
+var oQuintoCar = new QuintoCar();
+var divListado = document.getElementById("listado");
+
+//Mostrar Formularios
+
+//Mostrar formulario cliente(anadir los otros formularios mientras los vais creando)
+function altaCliente() {
+    frmAltaCliente.style.display = "block";
+    divListado.style.display = "none";
+}
+
+
+
+//Funciones 
+
+function aceptarAltaCliente() {
+    // Recoger valores del formulario
+    let sNif = frmAltaCliente.txtNif.value.trim();
+    let sNombre = frmAltaCliente.txtNombre.value.trim();
+    let sApellido=frmAltaCliente.txtApellidos.value.trim();
+    let iTelefono = parseInt(frmAltaCliente.txtTelefono.value.trim());
+
+    // Creamos el objeto paciente
+    let oCliente = new Cliente(sNif, sNombre,sApellido, iTelefono);
+   
+    // Alta de paciente en el hospital
+    let sMensaje = oQuintoCar.altaCliente(oCliente);
+
+    alert(sMensaje);
+}
+
+
+
+
---
+++ QuintoCar/objetos.js
@@ -0,0 +1,44 @@
+"use strict";
+//Clase Cliente
+function Cliente(sNIF, sNombre, sApellido, iTelefono) {
+    this.nif = sNIF;
+    this.nombre= sNombre;
+    this.apellido = sApellido;
+    this.telefono = iTelefono;
+}
+Cliente.prototype.toString = function() {
+    let sMensaje = "El cliente " +this.nombre+ " " +this.apellido+" con DNI " +this.nif+ " y telefono " +this.telefono;
+    return sMensaje;
+}
+
+
+
+//Clase principal
+class QuintoCar{
+
+constructor() 
+    {
+        this.clientes = [];
+        this.ventas = [];
+        this.compras = [];
+        this.vehiculos = [];
+    }
+
+    //Introducir cliente
+    altaCliente(oCliente) 
+    {
+        let sMensaje = "";
+
+        if (this.clientes.filter(clientes => clientes.nif == oCliente.nif).length == 0) {
+            //No se encuentra --> lo doy de alta
+            this.clientes.push(oCliente);
+            sMensaje = "Alta cliente OK";
+        } else {
+            sMensaje = "El cliente ya estaba registrado";
+        }
+
+        return sMensaje;
+      
+    }
+
+}
